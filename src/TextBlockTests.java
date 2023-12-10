a16f2d8b325e199b5dd9984f61a77b4fe76c4fc7
Kevin Fitzgerald <143578317+KFitz1123@users.noreply.github.com>
2023-12-09 18:41:13 -0600

    rename TextBoxTests.java to TextBlockTests.java

--- src/TextBlockTests.java
+++ src/TextBlockTests.java
@@ -3,7 +3,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 
-public class TextBoxTests {
+public class TextBlockTests {
   /*
    * Author: Kevin Fitzgerald
    * 
